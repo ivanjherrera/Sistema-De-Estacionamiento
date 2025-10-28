721f9f4b05e06c579340c308ae476108ca3f81a6
Iván Herrera <ivanjherrera56@gmail.com>
2025-10-28 17:17:18 -0600

    Inicializacion del proyecto de estacionamiento

---
+++ estacionamiento-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "../src/output.css"
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind funcionando 😎
+      </h1>
+    </div>
+  );
+}
+
+export default App;
